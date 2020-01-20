4214f6c8d151bd1a949e0ef11f0861d3be3c75a8
Greg Wagner <gpwagner@gmail.com>
2020-01-20 22:37:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,12 +10,22 @@ def nyc_pigeon_organizer(data)
           if !pigeons_by_name[name]
             pigeons_by_name[name] = {}
           end
+<<<<<<< HEAD
         if !pigeons_by_name[name][key]
           pigeons_by_name[name][key] = []
                 end
+=======
+          if !pigeons_by_name[name][key]
+            pigeons_by_name[name][key] = []
+          end
+>>>>>>> 6054d7d58adf7a539bc38c837480e32b9f87dc21
           pigeons_by_name[name][key] << new_value.to_s
         end
       end
     end
+<<<<<<< HEAD
+=======
+    binding.pry
+>>>>>>> 6054d7d58adf7a539bc38c837480e32b9f87dc21
     pigeons_by_name
   end
