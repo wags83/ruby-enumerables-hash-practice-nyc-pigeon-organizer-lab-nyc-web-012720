736d3bd17c41528a6bde280ba62baefcc6006f99
Greg Wagner <gpwagner@gmail.com>
2020-01-20 06:41:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+    pigeons_by_name = {}
+    data.each do |key, value|
+      value.each do |new_value, names|
+        names.each do |name|
+          if !pigeons_by_name[name]
+            pigeons_by_name[name] = {}
+          end
+        if !pigeons_by_name[name][key]
+          pigeons_by_name[name][key] = []
+                end
+          pigeons_by_name[name][key] << new_value.to_s
+        end
+      end
+    end
+    pigeons_by_name
+  end
